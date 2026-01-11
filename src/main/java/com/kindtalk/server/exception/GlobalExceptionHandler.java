829814e2d170414e2f988c37b152e71bfe3fa3f3
zxc011797 <zxcz011797@gmail.com>
2026-01-11 19:57:06 +0900

    feat: add BadGatewayException and global validation error handler

--- src/main/java/com/kindtalk/server/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kindtalk/server/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.kindtalk.server.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +24,25 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(
       new ErrorResponse(HttpStatus.NOT_FOUND, e.getMessage()),
       HttpStatus.NOT_FOUND
+    );
+  }
+
+  @ExceptionHandler(value = BadGatewayException.class)
+  public ResponseEntity<ErrorResponse> handleBadGatewayException(
+    BadGatewayException e) {
+    return new ResponseEntity<>(
+      new ErrorResponse(HttpStatus.BAD_GATEWAY, e.getMessage()),
+      HttpStatus.BAD_GATEWAY
+    );
+  }
+
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+    MethodArgumentNotValidException e) {
+    String message = e.getAllErrors().get(0).getDefaultMessage();
+    return new ResponseEntity<>(
+      new ErrorResponse(HttpStatus.BAD_REQUEST, message),
+      HttpStatus.BAD_REQUEST
     );
   }
 }
