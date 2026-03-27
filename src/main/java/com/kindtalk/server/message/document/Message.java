42e608c04aa79794110f344d0b6975fb1dbec46e
ensalada.de.pollo <ensalada.de.pollo@90.local>
2026-03-27 21:37:15 +0900

    typo: correct typo

--- src/main/java/com/kindtalk/server/message/document/Message.java
+++ src/main/java/com/kindtalk/server/message/document/Message.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "chat_message")
 @CompoundIndexes({
   @CompoundIndex(name = "idx_room_sendat_desc", def = "{'roomId': 1, 'sendAt': -1}"),
-  @CompoundIndex(name = "idx_topic_sendat_asc", def = "{'topicId': 1, 'sendAt': 1, sparse = true")
+  @CompoundIndex(name = "idx_topic_sendat_asc", def = "{'topicId': 1, 'sendAt': 1, sparse = true}")
 })
 public class Message {
 
